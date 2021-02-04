ddcc210913c93672357868237c60e00669330771
Field <field.manar@gmail.com>
2021-02-04 09:08:42 -0500

    docstring

--- legends.py
+++ legends.py
@@ -1,3 +1,29 @@
+"""Random Legend Picker
+
+This script chooses random legends for a whole squad. Player names can be
+supplied as command line arguments, and legends will be matched to them.
+
+Invocation:
+    python legends.py [PLAYER]...
+
+Example:
+    Without arguments the script picks three legends:
+
+        $ python legends.py
+        Random legends! Loba! Horizon! Octane!
+        
+    With arguments the script picks legends for each player supplied:
+
+        $ python legends.py Frodo Sam
+        Random legends!
+        Frodo plays Mirage!
+        Sam plays Gibraltar!
+
+Notes:
+    If Skyler is playing, Crypto is removed from the legend pool.
+    
+
+"""
 import random
 import sys
 
