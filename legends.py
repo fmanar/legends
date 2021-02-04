e8b23f68ee9328be520b62e6f91f746da0d64364
Field\laptop <field.manar@gmail.com>
2021-02-04 08:55:00 -0500

    cli args for player names

--- legends.py
+++ legends.py
@@ -1,9 +1,18 @@
 import random
+import sys
 
 legends = ['Bloodhound', 'Gibraltar', 'Lifeline', 'Pathfinder', 'Wraith',
     'Bangalore', 'Caustic', 'Mirage', 'Octane', 'Wattson',
     'Crypto', 'Revnant', 'Loba', 'Rampart', 'Horizon']
 
-legends.remove('Crypto')
+if 'Skyler' in sys.argv:
+    legends.remove('Crypto')
+
 random.shuffle(legends)
-print(f'Random legends! {legends[0]}! {legends[1]}! {legends[2]}!')
+
+if len(sys.argv) > 1:
+    print('Random legends!')
+    for player, legend in zip(sys.argv[1:], legends):
+        print(f'{player} plays {legend}!')
+else:
+    print(f'Random legends! {legends[0]}! {legends[1]}! {legends[2]}!')
